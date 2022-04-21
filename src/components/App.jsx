9e80d1566c7b797d98fe57a111e7db8874a2a7b2
Anmol Khandelwal <a.khandelwal2672001@gmail.com>
2022-04-21 15:58:55 +0530

    Props practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import Card from "./Card";
 
 function App()
 {
     return( 
     <div>
         <Header />
-        <Note />
+        {/* <Note /> */}
+        <Card name="Anmol" src="https://picsum.photos/200" tel="123445678" email="a.kh@Tech.com"/>
+        <Card name="vaibhav" src="https://picsum.photos/200" tel="846445678" email="v.an@Tech.com"/>
         <Footer />
     </div>
     );
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card(props)
+{
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <img 
+            src={props.src}
+            alt="img"
+            />
+            <p>{props.tel}</p>
+            <p>{props.email}</p>
+        </div>
+    );
+}
+
+export default Card;
