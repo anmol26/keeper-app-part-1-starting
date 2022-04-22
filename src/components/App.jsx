6378664e3541ef93887498aec1759e2cb42a5127
Anmol Khandelwal <a.khandelwal2672001@gmail.com>
2022-04-22 12:31:16 +0530

    Replace function by using Arrow functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
-import Card from "./Card";
+//import Card from "./Card";
 import notes from "../notes"
 
-function CreateNotes(noteItem){
-    return(
-        <Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>
-    );
-}
+// function CreateNotes(noteItem){
+//     return(
+//         <Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>
+//     );
+// }
 
 function App()
 {
     return( 
     <div>
         <Header />
-        {notes.map(CreateNotes)}
-        {/* {notes.map( noteItem => (<Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>))} */}
+        {/* {notes.map(CreateNotes)} */}
+        {notes.map( noteItem => (<Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>))}
         {/* <Card name="Anmol" src="https://picsum.photos/200" tel="123445678" email="a.kh@Tech.com"/>
         <Card name="vaibhav" src="https://picsum.photos/200" tel="846445678" email="v.an@Tech.com"/> */}
         <Footer />
