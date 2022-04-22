a8a17184426aaca0d323e882489db56cfd1b31f0
Anmol Khandelwal <a.khandelwal2672001@gmail.com>
2022-04-22 16:49:57 +0530

    Hooks Implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
 //import Card from "./Card";
 import notes from "../notes"
+import Time from "./Time"
 
 // function CreateNotes(noteItem){
 //     return(
@@ -13,6 +14,13 @@ import notes from "../notes"
 
 function App()
 {
+    let now= new Date().toLocaleTimeString();
+    const [currentTime,setTime]= useState(now);
+
+    function updateTime(){
+        return(setTime(now));
+    }
+
     return( 
     <div>
         <Header />
@@ -20,7 +28,11 @@ function App()
         {notes.map( noteItem => (<Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>))}
         {/* <Card name="Anmol" src="https://picsum.photos/200" tel="123445678" email="a.kh@Tech.com"/>
         <Card name="vaibhav" src="https://picsum.photos/200" tel="846445678" email="v.an@Tech.com"/> */}
+
+        <Time currentTime={currentTime}/>
+        <button onClick={updateTime}>Click Me to Get CurrentTime</button>
         <Footer />
+
     </div>
     );
 }
