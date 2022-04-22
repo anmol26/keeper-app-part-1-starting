ce8762b56b9b3f43e777d6759738f272c654fdb0
Anmol Khandelwal <a.khandelwal2672001@gmail.com>
2022-04-22 12:28:52 +0530

    Keeper-app-part-2-completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,23 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
 import Card from "./Card";
+import notes from "../notes"
+
+function CreateNotes(noteItem){
+    return(
+        <Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>
+    );
+}
 
 function App()
 {
     return( 
     <div>
         <Header />
-        {/* <Note /> */}
-        <Card name="Anmol" src="https://picsum.photos/200" tel="123445678" email="a.kh@Tech.com"/>
-        <Card name="vaibhav" src="https://picsum.photos/200" tel="846445678" email="v.an@Tech.com"/>
+        {notes.map(CreateNotes)}
+        {/* {notes.map( noteItem => (<Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>))} */}
+        {/* <Card name="Anmol" src="https://picsum.photos/200" tel="123445678" email="a.kh@Tech.com"/>
+        <Card name="vaibhav" src="https://picsum.photos/200" tel="846445678" email="v.an@Tech.com"/> */}
         <Footer />
     </div>
     );
